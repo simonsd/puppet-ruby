fbc47d10c9f4ab7e586b6920d41528287f4196fa
Dave Simons <dave@inuits.be>
2011-06-03 14:37:46 +0200

    split up init.pp Signed-off-by: Dave Simons <dave@inuits.be>

---
+++ manifests/packages.pp
@@ -0,0 +1,40 @@
+class ruby::packages {
+	package { ruby:
+		ensure => installed,
+		name => $operatingsystem ? {
+			Centos => 'ruby',
+			Debian => 'ruby',
+		},
+	}
+
+
+	package { rubydev:
+		ensure => installed,
+		name => $operatingsystem ? {
+			Centos => 'ruby-devel',
+			Debian => 'ruby-dev',
+		},
+		require => Package['ruby'],
+	}
+
+	package { rubygems:
+		ensure => installed,
+		name => $operatingsystem ? {
+			Centos => 'rubygems',
+			Debian => 'rubygems',
+		},
+		require => $operatingsystem ? {
+			Centos => [ Package['ruby'], Exec['epel'] ],
+			Debian => Package['ruby'],
+		},
+	}
+
+	package { rubydocs:
+		ensure => installed,
+		name => $operatingsystem ? {
+			Centos => 'ruby-docs',
+			Debian => 'ruby',
+		},
+		require => Package['ruby'],
+	}
+}
---
+++ manifests/repo.pp
@@ -0,0 +1,9 @@
+class ruby::repo {
+	yumrepo { "kbs-el5-rb187":
+			descr => "kbs-el5-rb187",
+			enabled=>1,
+			baseurl=> "http://centos.karan.org/el5/ruby187/i386",
+			gpgcheck=>1,
+			gpgkey=> "http://centos.karan.org/RPM-GPG-KEY-karan.org.txt",
+	}
+}
