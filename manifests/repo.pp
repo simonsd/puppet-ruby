ea3e6cb18b5420f50bfc8b4a3fb1f3434437c1a0
Dave Simons <dave@inuits.be>
2011-06-14 15:36:20 +0200

    change repo to 64-bit! Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -2,7 +2,7 @@ class ruby::repo {
 	yumrepo { "kbs-el5-rb187":
 			descr => "kbs-el5-rb187",
 			enabled=>1,
-			baseurl=> "http://centos.karan.org/el5/ruby187/i386",
+			baseurl=> "http://centos.karan.org/el5/ruby187/x86_64",
 			gpgcheck=>1,
 			gpgkey=> "http://centos.karan.org/RPM-GPG-KEY-karan.org.txt",
 	}
