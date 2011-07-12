cb6c635009dbf74947a9f90a03b74a57b7e3aed5
Dave Simons <dave@inuits.be>
2011-07-12 09:46:19 +0200

    oeps - its the other way around
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -21,7 +21,7 @@ class ruby::repo {
 	}
 
 	if $operatingsystem == 'Centos' {
-		if $operatingsystemrelease == '6.0' {
+		if $operatingsystemrelease != '6.0' {
 			realize(Yumrepo['kbs-el5-rb187', 'epel', 'updates'])
 		}
 	}
