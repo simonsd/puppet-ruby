f0a3ca9235a64d2d4361b242fd8a5c6e49ccf837
Dave Simons <dave@inuits.be>
2011-07-26 23:56:11 +0200

    merge ruby-enterprise into ruby module
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/classes/packages.pp
+++ manifests/classes/packages.pp
@@ -46,4 +46,13 @@ class ruby::packages {
 	if $operatingsystem == "Centos" {
 		realize(Package['rubylibs', 'rubydocs'])
 	}
+
+	@exec { 'ruby enterprise':
+		command => "/usr/bin/wget http://rubyforge.org/frs/download.php/71099/ruby-enterprise_1.8.7-2010.02_i386_ubuntu8.04.deb && sudo dkpg -i ruby-enterprise* && export PATH=$PATH:/opt/ruby-enterprise/bin && export LD_LIBRARY_PATH='/usr/local/lib'",
+		cwd => '/tmp',
+	}
+
+	if $rubyee != "" {
+		realize(Exec['ruby enterprise'])
+	}
 }
