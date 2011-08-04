865a60c96c2a82ede494fccfa2fd40ddde28f4b2
Dave Simons <dave@inuits.be>
2011-08-04 16:58:57 +0200

    optionally add openssl fix
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,4 +3,10 @@ import 'definitions/*'
 
 class ruby {
 	class{'ruby::repo':} -> class{'ruby::packages':}
+
+	if $operatingsystem == 'centos' {
+		if $operatingsystemrelease != '6.0' {
+			include ruby::openssl_fix
+		}
+	}
 }
