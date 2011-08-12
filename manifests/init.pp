24ecf74353a559aa2a478ccb59edcba0e6f23d8c
Dave Simons <dave@inuits.be>
2011-08-12 14:11:49 +0200

    add params to ruby class
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,22 @@
 import 'classes/*'
 import 'definitions/*'
 
-class ruby {
-	class{'ruby::repo':} -> class{'ruby::packages':}
+class ruby (
+	$stages = 'no',
+	$home = '/usr',
+	$lib_dir = '/usr/lib64/ruby',
+	$bin_dir = '/usr/bin'
+) {
+	if $stages != 'yes' {
+		class{'ruby::repo':} -> class{'ruby::packages':}
+	} else {
+		class{
+			'ruby::repo':
+				stage => repo;
+			'ruby::packages':
+				stage => depends;
+		}
+	}
 
 	if $operatingsystem == 'centos' {
 		if $operatingsystemrelease != '6.0' {
