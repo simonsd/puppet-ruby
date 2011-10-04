f54f901ed72816a8ffd507b3af15ae087feb04c1
Dave Simons <dave@inuits.be>
2011-10-04 12:11:37 +0200

    rm repo defs, use repo module instead
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/classes/repo.pp
+++ manifests/classes/repo.pp
@@ -1,32 +1,4 @@
 class ruby::repo {
-	@yumrepo {
-		'kbs-el5-rb187':
-			descr => "kbs-el5-rb187",
-			enabled =>1,
-			baseurl => "http://centos.karan.org/el5/ruby187/$::hardwaremodel",
-			gpgcheck =>1,
-			gpgkey => "http://centos.karan.org/RPM-GPG-KEY-karan.org.txt";
-
-		'epel':
-			descr => 'EPEL',
-			enabled => 1,
-			baseurl => $::operatingsystem ? {
-				default => undef,
-				'Centos' => $::operatingsystemrelease ? {
-					'5.*' => "http://mirror.eurid.eu/epel/5/$::hardwaremodel/",
-					'6.0' => "http://mirror.eurid.eu/epel/6/$::hardwaremodel/",
-				},
-			},
-			gpgcheck => 0;
-
-		"updates":
-			baseurl => absent,
-			mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates',
-			enabled => 1,
-			exclude => "ruby*",
-			gpgkey => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-5';
-	}
-
 	if $::operatingsystem == 'Centos' {
 		if $::operatingsystemrelease != '6.0' {
 			realize(Yumrepo['kbs-el5-rb187', 'updates'])
