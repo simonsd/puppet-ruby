7c74c5d29fa048d47b0d54a14c34fac44e184373
Dave Simons <dave@inuits.eu>
2012-05-20 19:16:21 +0200

    pacman provider in puppet core is now yaourt-aware, so no need for the dirty hacks like running execs to install pkgs with yaourt
    Signed-off-by: Dave Simons <dave@inuits.eu>

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -2,7 +2,10 @@ class ruby::packages {
   @package {
     'ruby':
       ensure  => latest,
-      name    => 'ruby',
+      name    => $::operatingsystem ? {
+        default   => 'ruby',
+        archlinux => 'ruby-1.8',
+      },
       require => $::operatingsystem ? {
         default => undef,
         Centos  => $::operatingsystemrelease ? {
@@ -21,7 +24,10 @@ class ruby::packages {
 
     'rubygems':
       ensure  => installed,
-      name    => 'rubygems',
+      name    => $::operatingsystem ? {
+        default   => 'rubygems',
+        archlinux => 'rubygems1.8',
+      },
       require => $::operatingsystem ? {
         default => Package['ruby'],
         Centos  => $::operatingsystemrelease ? {
@@ -56,27 +62,14 @@ class ruby::packages {
       command => "/usr/bin/wget http://rubyforge.org/frs/download.php/71099/ruby-enterprise_1.8.7-2010.02_i386_ubuntu8.04.deb && sudo dkpg -i ruby-enterprise* && export PATH = $PATH:/opt/ruby-enterprise/bin && export LD_LIBRARY_PATH = '/usr/local/lib'",
       cwd     => '/tmp';
 
-    'ruby1.8-aur':
-      command  => 'yaourt -S --noconfirm ruby1.8',
-      provider => shell,
-      unless   => 'yaourt -Qi ruby1.8',
-      require  => Class['arch::yaourt'];
-
-    'rubygems1.8-aur':
-      command  => 'yaourt -S --noconfirm rubygems1.8',
-      provider => shell,
-      unless   => 'yaourt -Qi rubygems1.8',
-      require  => Exec['ruby1.8-aur'];
-
     'ruby1.8_PATH':
       command  => 'sed -i \'s/^PATH=\\"/PATH=\\"\\/opt\\/ruby1.8\\/bin:/\' /etc/profile',
       unless   => 'grep \'^PATH="/opt/ruby1.8/bin:\' /etc/profile',
-      provider => shell,
-      require  => Exec['ruby1.8-aur'];
+      provider => shell;
   }
 
   if $::operatingsystem == 'archlinux' {
-    realize(Exec['ruby1.8-aur', 'rubygems1.8-aur', 'ruby1.8_PATH'])
+    realize(Exec['ruby1.8_PATH'])
   }
 
   if $ruby::rubyee != '' {
