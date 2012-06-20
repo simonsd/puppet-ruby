6b2071126dd9172922eb32ee82fd6b805e86990f
Dave Simons <dave@inuits.eu>
2012-06-20 18:39:26 +0200

    fix repo resource to work on all 6.x rhel distros, rather than only 6.0
    Signed-off-by: Dave Simons <dave@inuits.eu>

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,6 +1,6 @@
 class ruby::repo {
   if $::operatingsystem == 'Centos' {
-    if $::operatingsystemrelease != '6.0' {
+    if $::operatingsystemrelease !~ '^6' {
       realize(Yumrepo['kbs-el5-rb187', 'updates'])
     }
     realize(Yumrepo['epel'], File['epel-gpg-key'])
